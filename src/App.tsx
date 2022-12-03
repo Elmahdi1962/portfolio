bd1db191ba1a932db03a3ea76e6a4a759753fb6f
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-12-03 13:18:37 +0100

    finished scrolling feature, and update styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,51 @@ import Skills from "./components/skills/Skills";
 import Contact from "./components/contact/Contact";
 import "./app.css";
 
+import { useEffect } from "react";
+
+export let clickedLink = "#home";
+
 function App() {
+  const scrollSnaper = (e: any | null) => {
+    e.preventDefault();
+    // get all nav bar links that link to an id
+    const links = Array.from(document.querySelectorAll('a[href^="#"]'));
+    // loop thru the anchors while keeping track of index
+    for (let i = 0; i < links.length; i++) {
+      // if found the link that was alredy clicked
+      if (links[i].getAttribute("href") === clickedLink) {
+        // if there is a next link in the list then click that next link
+        // and update clicked link variable
+        if (i + 1 < links.length) {
+          clickedLink = links[i + 1].getAttribute("href") || "";
+          document?.querySelector(clickedLink)?.scrollIntoView({
+            behavior: "smooth",
+          });
+        } else {
+          // else click the first link
+          clickedLink = links[0].getAttribute("href") || "";
+          document?.querySelector(clickedLink)?.scrollIntoView({
+            behavior: "smooth",
+          });
+        }
+        break;
+      }
+    }
+  };
+
+  useEffect(() => {
+    // get scrollable elm
+    const elm = document.getElementById("content-container");
+    // add scroll event listener
+    (elm as HTMLElement).addEventListener("mousewheel", scrollSnaper);
+
+    // remove listener on cleanUp
+    return document.removeEventListener("mousewheel", scrollSnaper);
+  }, []);
   return (
     <div className="App">
       <Navbar />
-      <div className="content-container">
+      <div id="content-container">
         <Home />
         <Skills />
         <Projects />
