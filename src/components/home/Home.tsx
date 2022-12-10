b4278e6694a20791c7e47d2f02a8865d3667c20c
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-12-10 13:09:31 +0100

    basic home section

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,7 +5,17 @@ export interface HomeProps {}
 export default function Home(props: HomeProps) {
   return (
     <section id="home" className="home-container">
-      <div className="home-wrapper">home</div>
+      <div className="home-wrapper">
+        <img src="images/mypic.jpg" alt="my picture" id="profile-picture" />
+        <h1>Software Engineer, Frontend Developer</h1>
+        <p>
+          I’m a self taugh programmer and an Alx-africa Software engineer
+          Program Graduate. i have a background in electrical installation, game
+          development, Agricultural and dairy farming. Seeking to get my dream
+          job as a Frontend developer.
+        </p>
+        <button>Download Resume</button>
+      </div>
     </section>
   );
 }
